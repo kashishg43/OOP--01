d1cd3a45f9e81573cc21178a8e039bf16e335a30
kashishgupta <kashishgupta884@gmail.com>
2023-03-15 11:47:02 +0000

    hi

---
+++ Coursework/cw-model_2022/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -0,0 +1,73 @@
+package uk.ac.bris.cs.scotlandyard.model;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import java.util.*;
+import javax.annotation.Nonnull;
+import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
+import uk.ac.bris.cs.scotlandyard.model.Move.*;
+import uk.ac.bris.cs.scotlandyard.model.Piece.*;
+import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.*;
+
+
+/**
+ * cw-model
+ * Stage 1: Complete this class
+ */
+public final class MyGameStateFactory implements Factory<GameState> {
+
+	private final class MyGameState implements GameState {
+		private GameSetup setup;
+		private Player mrX;
+		private ImmutableList<Player> detectives;
+		@Override public GameSetup getSetup() {
+			this.setup = setup;
+			this.mrX = mrX;
+			this.detectives = detectives;
+			return setup;
+		}
+		@Override  public ImmutableSet<Piece> getPlayers() {
+
+
+		}
+
+		@Nonnull
+		@Override
+		public Optional<Integer> getDetectiveLocation(Detective detective) {
+			return null;
+		}
+
+		@Nonnull
+		@Override
+		public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+			return null;
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableList<LogEntry> getMrXTravelLog() {
+			return null;
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableSet<Piece> getWinner() {
+			return null;
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableSet<Move> getAvailableMoves() {
+			return null;
+		}
+
+		@Override public GameState advance(Move move) {  return null;  }
+	}
+
+	@Nonnull @Override public GameState build(
+			GameSetup setup,
+			Player mrX,
+			ImmutableList<Player> detectives) {return null;}
+
+
+}
